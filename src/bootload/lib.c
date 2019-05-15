1af82ccf9bb3c88176759112656294c570dda9f8
yokenzan <31175068+yokenzan@users.noreply.github.com>
2019-05-16 01:00:59 +0900

    2ndステップ - シリアル通信

--- src/bootload/lib.c
+++ src/bootload/lib.c
@@ -2,6 +2,89 @@
 #include "serial.h"
 #include "lib.h"
 
+
+void *memset(void *b, int c, long len)
+{
+    char *p;
+
+    for(p = b; len > 0; len--)
+        *(p++) = c;
+
+    return b;
+}
+
+
+void *memcpy(void *dst, const void *src, long len)
+{
+    char *d = dst;
+    const char *s = src;
+
+    for(; len > 0; len--)
+        *(d++) = *(s++);
+
+    return dst;
+}
+
+
+int memcmp(const void *b1, const void *b2, long len)
+{
+    const char *p1 = b1, *p2 = b2;
+
+    for(; len > 0; len--) {
+        if(*p1 != *p2) return (*p1 > *p2) ? 1 : -1;
+        p1++;
+        p2++;
+    }
+
+    return 0;
+}
+
+
+int strlen(const char *s)
+{
+    int len;
+
+    for(len = 0; *s; s++, len++)
+
+        ;
+    return len;
+}
+
+
+char *strcpy(char *dst, const char *src)
+{
+    char *d = dst;
+
+    for(;; dst++, src++) {
+        *dst = *src;
+        if(!*src) break;
+    }
+
+    return d;
+}
+
+
+int strcmp(const char *s1, const char *s2)
+{
+    while(*s1 || *s2) {
+        if(*s1 != *s2) return (*s1 > *s2) ? 1 : -1;
+    }
+}
+
+
+int strncmp(const char *s1, const char *s2, int len)
+{
+    while((*s1 || *s2) && (len > 0)) {
+        if(*s1 != *s2) return (*s1 > *s2) ? 1 : -1;
+        s1++;
+        s2++;
+        len--;
+    }
+
+    return 0;
+}
+
+
 /* 一文字送信 */
 int putc(unsigned char c)
 {
@@ -17,6 +100,36 @@ int puts(unsigned char *str)
 {
     while(*str)
         putc(*(str++));
+
+    return 0;
+}
+
+
+/* 数値の16進数表示 */
+int putxval(unsigned long value, int column)
+{
+    /* 2進数表記32桁(32bit)は16進数表記8桁(32bit / 4bit) */
+    char buf[9];
+    char *p;
+
+    /* 下の桁から処理するので、バッファの終端から利用する */
+    p      = buf + sizeof(buf) - 1;
+    *(p--) = '\0';
+
+    if(!value && !column) column++;
+
+    while(value || column) {
+        *(p--) = "0123456789abcdef"[value & 0xf];
+        value >>= 4;
+        if(column) column--;
+    }
+
+    /*
+     * whileの中で文字代入後にデクリメントしているため、
+     * ループを抜けた直後は戻りすぎた状態になっているので、
+     * ポインタ演算で1バイト(char *)分すすめる
+     */
+    puts((unsigned char *)p + 1);
 
     return 0;
 }
